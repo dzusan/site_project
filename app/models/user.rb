708553512d48d456b455be5b08bc3057a6694f56
Sergey Lenkov <dzusan_creator@yahoo.com>
2011-11-17 01:27:24 +0400

    Pass this work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessible :name, :email, :password, :password_confirmation
+
+  has_secure_password
+  #self.salt = Digest::SHA2.hexdigest("#{Time.now.utc}") if !self.exists?
+  validates_presence_of :password, :on => :create
 
   email_regex = /\A[\w+\-.]+@+[a-z\d\-.]+[a-z]+\z/i
 
@@ -10,4 +14,6 @@ class User < ActiveRecord::Base
   validates :email, :presence   => true,
                     :format     => {:with => email_regex},
                     :uniqueness => {:case_sensitive => false}
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 SiteProject::Application.routes.draw do
 
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
-  match '/signup',    :to => 'users#new'
+    match '/signup',  :to => 'users#new'
+    match '/signin',  :to => 'sessions#new'
+    match '/signout', :to => 'sessions#destroy'
 
   get "users/new"
 
-    match '/signup',  :to => 'users#new'
 
   get "blog/home"
 
